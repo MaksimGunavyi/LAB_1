2bd4a68cd1dd3ecfe82d707e35cd585b968fac67
MaksimGunavyi <112701391+MaksimGunavyi@users.noreply.github.com>
2023-02-16 10:39:41 +0200

    LAB_1

---
+++ Lab1_3/Lab1_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LAB1_3
+
+{
+    public delegate int Number();
+    public delegate int MediumCalc(Number[] arrayX);
+    class Program
+    {
+        static void Calculate(int a, int b, int c, out double aa, out double bb, out double cc)
+        {
+            aa = a / 5;
+            bb = b / 5;
+            cc = c / 5;
+        }
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            double aa, bb, cc;
+            Console.WriteLine("Введите 1 значение");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2 значение");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 3 значение");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            Calculate(a, b, c, out aa, out bb, out cc);
+
+
+
+        }
+    }
+}
